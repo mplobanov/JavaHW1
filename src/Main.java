1667bcc8cee2dd85bcd34410922fb7c9401a4ced
Mikhail Lobanov <mishalob2002@gmail.com>
2024-03-31 15:41:55 +0300

    stage2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws StudentException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Student st1 = new Student("Mike");
-        Student st2 = new Student("Alex", new ArrayList<>(List.of(2, 3, 3)));
-
+        Student st1 = new Student<String>("Mike");
+        Student st2 = new Student<Integer>("Alex", new ArrayList<>(List.of(2, 4)) ,(grade) -> grade % 2 == 0);
+//        st2.addGrade(3);
+        st1.addGrade("Bingo Bongo");
         System.out.println(st1);
         System.out.println(st2);
     }
--- src/Student.java
+++ src/Student.java
@@ -1,34 +1,61 @@
 import java.util.ArrayList;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class Student {
+public class Student<Grade>  {
     private String name;
-    private ArrayList<Integer> grades;
+    private final ArrayList<Grade> grades;
 
+    private Predicate<Grade> isGradeValid;
 
-    public Student(String name) {
+    public Student(String name) throws StudentException {
+        if (!name.matches("[A-Z][a-zA-Z]*")) {
+            throw new StudentException("Incorrect name");
+        }
         this.name = name;
         this.grades = new ArrayList<>();
     }
 
-    public Student(String name, ArrayList<Integer> grades) {
+    public Student(String name, Predicate<Grade> isGradeValid) throws StudentException {
+        this(name);
+        this.isGradeValid = isGradeValid;
+    }
+
+    public Student(String name, ArrayList<Grade> grades) throws StudentException {
+        if (!name.matches("[A-Z][a-zA-Z]*")) {
+            throw new StudentException("Incorrect name");
+        }
         this.name = name;
         this.grades = grades;
+    }
+
+    public Student(String name, ArrayList<Grade> grades, Predicate<Grade> isGradeValid) throws StudentException  {
+        this(name, grades);
+        this.isGradeValid = isGradeValid;
+        if (grades.stream().anyMatch((grade) -> !isGradeValid.test(grade))) {
+            throw new StudentException("Incorrect grades upon construction");
+        }
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) throws StudentException {
+        if (!name.matches("[A-Z][a-zA-Z]*")) {
+            throw new StudentException("Incorrect name");
+        }
         this.name = name;
     }
 
-    public void addGrade(Integer grade) {
+    public void addGrade(Grade grade) throws StudentException {
+        if (isGradeValid != null && !isGradeValid.test(grade)) {
+            throw new StudentException("Incorrect grade");
+        }
         grades.add(grade);
     }
 
-    public ArrayList<Integer> getGrades() {
+    public ArrayList<Grade> getGrades() {
         return grades;
     }
 
